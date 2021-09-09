e1351fa24616fe519b1a505d9a6c8c9ab4256229
kinppink <648270560@qq.com>
2021-09-09 17:26:10 +0800

    deploy

---
+++ precache-manifest.4f81c50ece7491053a1a7668ba139339.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae6cebbd6883a4fee79",
+    "url": "/Vue-ToDo/css/app.c2ce4e43.css"
+  },
+  {
+    "revision": "3a97a10ff285801847cd",
+    "url": "/Vue-ToDo/css/chunk-vendors.2910b850.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/Vue-ToDo/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/Vue-ToDo/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "7706c3a7ebbd5a2cb661512a30e9ac5c",
+    "url": "/Vue-ToDo/index.html"
+  },
+  {
+    "revision": "0ae6cebbd6883a4fee79",
+    "url": "/Vue-ToDo/js/app.897387bf.js"
+  },
+  {
+    "revision": "3a97a10ff285801847cd",
+    "url": "/Vue-ToDo/js/chunk-vendors.fb60cc7a.js"
+  },
+  {
+    "revision": "6013d8f0f8a94e2084a4ca8055896afa",
+    "url": "/Vue-ToDo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-ToDo/robots.txt"
+  }
+]);
